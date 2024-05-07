5676a4ee5769e1b7aa63f31f261f5d03825ce6f0
faragzz <a.khaled46462@gmail.com>
2024-05-08 00:35:44 +0300

    chore:CookieProvidor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import  NavBar  from "./components/navBar";
+import { CookiesProvider } from "next-client-cookies/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* <NavBar/> */}
-        {children}
+        <CookiesProvider>{children}</CookiesProvider>
         </body>
     </html>
   );
